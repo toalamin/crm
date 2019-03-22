c61a7b8dea12a5358383e3871cf9213c21bcc7e5
toalamin <toalaminbd@gmail.com>
2019-03-22 22:33:07 +0600

    leads table added

---
+++ database/migrations/2019_03_22_154831_create_leads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInterger('meeting_id');
+            $table->string('status',32);
+            $table->foreign('meeting_id')->references('id')->on('meetings')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leads');
+    }
+}
