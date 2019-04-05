7a951a3e2fed6560f4af7a452db6caa07b07dc37
toalamin <toalaminbd@gmail.com>
2019-04-05 17:59:31 +0600

    expense table added

---
+++ app/Models/Expense.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    
+    protected $guarded = [''];
+
+    public $timestamps = false;
+
+    protected $dates = ['created_at'];
+    
+    
+    
+    
+    
+}
